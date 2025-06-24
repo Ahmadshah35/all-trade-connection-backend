6ac29dc7458321cee029caa766c81abc908d51af
ahmad shah <ahmadshah102005@gmail.com>
2025-06-24 20:58:07 +0500

    mailer.js

--- helper/mailer.js
+++ helper/mailer.js
@@ -18,11 +18,11 @@ async function sendMail(userData) {
       from: process.env.SMTP_MAIL,
       to: userData.email,
       subject: "Your OTP for Account Verification - All-Trade-Connection",
-      text: `Your One-Time Password (OTP) is ${userData.Otp}. Please use this code to verify your account. .`,
+      text: `Your One-Time  (OTP) is ${userData.Otp}. Please use this code to verify your account. .`,
       html: `
               <div style="font-family: Arial, sans-serif; line-height: 1.6;">
                 <h2>Welcome to All-Trade-Connection!</h2>
-                <p>Your One-Time Password (OTP) is:</p>
+                <p>Your One-Time (OTP) is:</p>
                 <h1 style="color: #2e86de;">${userData.Otp}</h1>
                 <p>Please enter this code in the app to verify your account.</p>
                 <p><strong>Note:</strong>  Do not share it with anyone.</p>
